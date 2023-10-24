307044a59b35a118196a1f235df2669106d2b94a
Ian Fraser <ian@glenborrodale.co.uk>
2023-10-24 09:30:31 +0100

    57

--- components/extraflame/extraflame.cpp
+++ components/extraflame/extraflame.cpp
@@ -110,8 +110,9 @@ namespace esphome
                        << value
                        << "\", \"attributes\": {\"unit_of_measurement\": \"number\", \"friendly_name\": \"" << this->dump_.current
                        << "\"}}";
+                  const char * c = s3.cstr();
 
-                   ESP_LOGD(TAG, s3.str());
+                   ESP_LOGD(TAG, c);
 
                     trigger->process(s3.str());
                   }
